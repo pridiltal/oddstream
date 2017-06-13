f927fc66eca8baa0ce72d6daaaca08744851dd94
priyangt <pritalagala@gmail.com>
2017-06-13 13:15:40 +1000

    edit code set_threshold

--- R/find_odd_streams.R
+++ R/find_odd_streams.R
@@ -12,6 +12,9 @@
 #' The default value is set to TRUE
 #' @param update_threshold A numerical value to indicated how often the threshold should be updated.
 #'  (After how many windows it need be updated)
+#' @param concept_drift If TRUE, The outlying threshold will be updated after each window. The default is set
+#' to FALSE
+#' @param trials Input for \code{set_outlier_threshold} function. Default value is set to 500.
 #' @return The indices of the outlying series in each window. For each window a plot is also produced on the current
 #' graphic device
 #' @seealso  \code{\link{extract_tsfeatures}}, \code{\link{get_pc_space}}, \code{\link{set_outlier_threshold}},
@@ -23,44 +26,46 @@
 #' @importFrom ggplot2 ggplot
 #' @examples
 #' #Generate training dataset
-#' set.seed(123)
-#' nobs = 500
-#' nts = 50
+#' set.seed(890)
+#' nobs = 250
+#' nts = 100
 #' train_data <- ts(apply(matrix(ncol = nts, nrow = nobs), 2, function(nobs){10 + rnorm(nobs, 0, 3)}))
 #' # Generate test stream with some outliying series
 #' nobs = 15000
 #' test_stream <- ts(apply(matrix(ncol = nts, nrow = nobs), 2, function(nobs){10 + rnorm(nobs, 0, 3)}))
-#' test_stream[200:1400, 20:25] = test_stream[200:1400, 20:25] * 2
-#' test_stream[3020:3550, 20:25] = test_stream[3020:3550, 20:25] * 1.5
-#' find_odd_streams(train_data, test_stream , plot_type = 'line', window_skip = 100)
+#' test_stream[360:1060, 20:25] = test_stream[360:1060, 20:25] * 1.75
+#' test_stream[2550:3550, 20:25] =  test_stream[2550:3550, 20:25] * 2
+#' find_odd_streams(train_data, test_stream , plot_type = 'line', trials = 100)
 #'
+#' # To get the PCplot
+#' #find_odd_streams(train_data, test_stream , plot_type = 'pcplot')
 #' @references Clifton, D. A., Hugueny, S., & Tarassenko, L. (2011). Novelty detection with multivariate
 #' extreme value statistics. Journal of signal processing systems, 65 (3),371-389.
 #'
 #'
-find_odd_streams <- function(train_data, test_stream, update_threshold = TRUE, update_threshold_freq, plot_type = c("line", 
-    "pcplot"), window_length = nrow(train_data), window_skip = window_length) {
-    
+find_odd_streams <- function(train_data, test_stream, update_threshold = TRUE, update_threshold_freq, plot_type = c("line",
+    "pcplot"), window_length = nrow(train_data), window_skip = window_length, concept_drift = FALSE, trials = 500 ) {
+
     train_features <- extract_tsfeatures(train_data)
     pc <- get_pc_space(train_features)
-    t <- set_outlier_threshold(pc$pcnorm)
+    t <- set_outlier_threshold(pc$pcnorm, trials = trials )
     start <- seq(1, nrow(test_stream), window_skip)
     end <- seq(window_length, nrow(test_stream), window_skip)
-    
+
     i <- 1
     while (i <= length(end)) {
         window_data <- test_stream[start[i]:end[i], ]
-        
+
         window_features <- extract_tsfeatures(window_data)
         pc_test <- scale(window_features, pc$center, pc$scale) %*% pc$rotation
         pctest <- pc_test[, 1:2]
         fhat_test <- ks::kde(x = pc$pcnorm, H = t$H_scv, compute.cont = TRUE, eval.points = pctest)
         outliers <- which(fhat_test$estimate < t$threshold_fnx)
-        cat("Outliers: ", outliers, "\n")
-        
+
+
         if (plot_type == "line") {
             par(pty = "s")
-            plot.ts(as.ts(window_data), plot.type = "single", main = paste("data from: ", start[i], " to: ", end[i]), ylim = c(0, 
+            plot.ts(as.ts(window_data), plot.type = "single", main = paste("data from: ", start[i], " to: ", end[i]), ylim = c(0,
                 range(train_data)[2] * 4), ylab = "Value")
             if (length(outliers) > 0) {
                 f <- function(x) {
@@ -71,19 +76,39 @@ find_odd_streams <- function(train_data, test_stream, update_threshold = TRUE, u
         }
         if (plot_type == "pcplot") {
             par(pty = "s")
-            plot(t$fhat2, drawpoints = FALSE, abs.cont = t$threshold_fnx, col = "red", add = F, xlim = range(t$fhat2$x[, 1]) * 
+            plot(t$fhat2, drawpoints = FALSE, abs.cont = t$threshold_fnx, col = "red", add = F, xlim = range(t$fhat2$x[, 1]) *
                 40, ylim = range(t$fhat2$x[, 2]) * 40, main = paste("data from: ", start[i], " to: ", end[i]))
             points(pctest[, 1], pctest[, 2], col = "grey", pch = 16)
-            
+
             if (length(outliers) > 0) {
                 points(pctest[outliers, 1], pctest[outliers, 2], col = "red", pch = 16)
                 text(pctest[outliers, 1], pctest[outliers, 2], labels = outliers, pos = 1, cex = 0.8, col = "black")
             }
-            
+
         }
-        
-        
+
+
+
+        if (length(outliers) > 0) {
+           cat("Outliers from: ", start[i], " to: ", end[i], ": ", outliers, "\n")
+        } else {
+           cat("Outliers from: ", start[i], " to: ", end[i], ": ", "NULL", "\n")
+        }
+
+        if (concept_drift == TRUE)
+        {
+          if (length(outliers) > 0) {
+             train_features <- window_features[-outliers, ]
+          } else {
+             train_features <- window_features
+          }
+
+          pc <- get_pc_space(train_features)
+          t <- set_outlier_threshold(pc$pcnorm, trials = trials)
+        }
+
         i <- i + 1
+
     }
     dev.off()
 }
--- R/set_outlier_threshold.R
+++ R/set_outlier_threshold.R
@@ -12,6 +12,7 @@
 #' \code{\link{plotpc}}
 #' @export
 #' @importFrom ks Hscv
+#' @importFrom ks kde
 #' @importFrom MASS mvrnorm
 #' @references Clifton, D. A., Hugueny, S., & Tarassenko, L. (2011). Novelty detection with multivariate extreme value statistics.
 #' Journal of signal processing systems, 65 (3),371-389.
@@ -25,26 +26,26 @@
 #' pc <- get_pc_space(features)
 #' threshold <- set_outlier_threshold(pc$pcnorm)
 #' threshold$threshold_fnx
-set_outlier_threshold <- function(pc_pcnorm, p_rate = 0.001, trials = 300) {
-    
+set_outlier_threshold <- function(pc_pcnorm, p_rate = 0.001, trials = 500) {
+
     # Calculating the density region for typical data
     H_scv <- ks::Hscv(x = pc_pcnorm)
     fhat2 <- ks::kde(x = pc_pcnorm, H = H_scv, compute.cont = TRUE)
-    
+
     # generating data to find the threshold value
     fun2 <- function(x) {
         return(MASS::mvrnorm(n = 1, mu = x, Sigma = H_scv))
     }
     m <- nrow(pc_pcnorm)
     xtreme_fx <- numeric(trials)
-    for (i in 1:trials) {
-        s <- sample(1:m, size = m, replace = T)
-        mean_s <- pc_pcnorm[s, ]
-        new_data_list <- lapply(as.list(data.frame(t(mean_s))), FUN = fun2)
-        new_data <- data.frame(matrix(unlist(new_data_list), nrow = m, byrow = T), stringsAsFactors = FALSE)
-        fhat <- kde(x = pc_pcnorm, H = H_scv, compute.cont = TRUE, eval.points = new_data)
-        xtreme_fx[i] <- min(fhat$estimate)
+    f_extreme <- function(tempt) {
+       s <- sample(1:m, size = m, replace = T)
+       fhat <- ks::kde(x = pc_pcnorm, H = H_scv, compute.cont = TRUE, eval.points = t(apply(pc_pcnorm[s, ],
+                                                                                            1, fun2)))
+       return(tempt <- min(fhat$estimate))
     }
+    xtreme_fx <- sapply(X = xtreme_fx, f_extreme)
+
     # op <- par(mfrow = c(2, 1)) hist(xtreme_fx) Apply Psi transformation
     k <- 1/(2 * pi)
     psi_trans <- ifelse(xtreme_fx < k, (-2 * log(xtreme_fx) - 2 * log(2 * pi))^0.5, 0)
@@ -55,6 +56,6 @@ set_outlier_threshold <- function(pc_pcnorm, p_rate = 0.001, trials = 300) {
     dm <- 1/(sqrt(2 * log(m)))
     t <- cm + y * dm
     threshold_fnx <- exp(-((t^2) + 2 * log(2 * pi))/2)
-    
+
     return(list(threshold_fnx = threshold_fnx, fhat2 = fhat2, H_scv = H_scv))
 }
